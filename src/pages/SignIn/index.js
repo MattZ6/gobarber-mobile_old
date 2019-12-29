110e2693abbf13b34348e18c2fc2c573eba76e2a
Matheus Zanin <matt_z6@hotmail.com>
2019-12-29 11:19:06 -0300

    chore: Initial project structure

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import Background from '../../components/Background';
+
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Text>Sign in</Text>
+      <Input
+        icon="call"
+        placeholder="Digite seu nome"
+        style={{ marginTop: 30 }}
+      />
+      <Button>Entrar</Button>
+    </Background>
+  );
+}
